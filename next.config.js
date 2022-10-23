6d15919e6fcf42285f3d2a1bc2f4e0db8a8eee5c
goeun <eun3903@gmail.com>
2022-10-24 03:07:49 +0900

    project setup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  compiler: {
+    emotion: {
+      sourceMap: true,
+      autoLabel: "dev-only",
+      labelFormat: "[local]",
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
