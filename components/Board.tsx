f3bce583e3785d8c6aa49f4ff9a7c87943a55b55
goeun <eun3903@gmail.com>
2022-11-02 22:25:30 +0900

    feat: Board에서 Square 출력시 반복문 두개를 사용하도록 수정

--- components/Board.tsx
+++ components/Board.tsx
@@ -7,28 +7,25 @@ type BoardProps = {
 
 const Board = ({ squares }: BoardProps) => {
     const renderSquare = (index: number) => {
-        return <Square index={index} value={squares[index]} />;
+        return <Square key={index} index={index} value={squares[index]} />;
     };
 
-    return (
-        <div>
-            <BoardRow>
-                {renderSquare(0)}
-                {renderSquare(1)}
-                {renderSquare(2)}
-            </BoardRow>
-            <BoardRow>
-                {renderSquare(3)}
-                {renderSquare(4)}
-                {renderSquare(5)}
-            </BoardRow>
-            <BoardRow>
-                {renderSquare(6)}
-                {renderSquare(7)}
-                {renderSquare(8)}
-            </BoardRow>
-        </div>
-    );
+    const boardRows = () => {
+        let rows = [];
+        let squareIndex = 0;
+        for (let i = 0; i < 3; i++) {
+            let squares = [];
+            for (let j = 0; j < 3; j++) {
+                squares.push(renderSquare(squareIndex));
+                squareIndex++;
+            }
+            rows.push(<BoardRow key={i}>{squares}</BoardRow>);
+        }
+
+        return rows;
+    };
+
+    return <div>{boardRows()}</div>;
 };
 
 const BoardRow = styled.div`
